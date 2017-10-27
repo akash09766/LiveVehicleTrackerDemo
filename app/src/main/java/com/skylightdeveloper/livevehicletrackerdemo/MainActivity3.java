dd4383e9ece8a377f834b34b2a91dbd0605845d9
akash09766 <wangalwar.akash@gmail.com>
2017-10-27 20:44:39 +0530

    PlaceAutocomplete

--- app/src/main/java/com/skylightdeveloper/livevehicletrackerdemo/MainActivity3.java
+++ app/src/main/java/com/skylightdeveloper/livevehicletrackerdemo/MainActivity3.java
@@ -5,8 +5,18 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 
 /**
  * Created by akash.wangalwar on 27/10/17.
@@ -15,6 +25,7 @@ import android.widget.Button;
 public class MainActivity3 extends AppCompatActivity implements View.OnClickListener{
 
     private Button mStartBut;
+    private String TAG = MainActivity3.class.getSimpleName();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,7 +41,8 @@ public class MainActivity3 extends AppCompatActivity implements View.OnClickList
     public void onClick(View view) {
 
 //        startNavigation();
-        startNavigation01();
+//        startNavigation01();
+        openAutocompleteActivity(100);
     }
 
     private void startNavigation01() {
@@ -43,5 +55,60 @@ public class MainActivity3 extends AppCompatActivity implements View.OnClickList
         Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
                 Uri.parse("http://maps.google.com/maps?saddr=19.161494,73.001781&daddr=19.228290,72.977003"));
         startActivity(intent);
+    }
+
+    private void openAutocompleteActivity(int requestCode) {
+        try {
+            // The autocomplete activity requires Google Play Services to be available. The intent
+            // builder checks this and throws an exception if it is not the case.
+            AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
+                    .setTypeFilter(Place.TYPE_COUNTRY)
+                    .setCountry("IN")
+                    .build();
+
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                    .setFilter(typeFilter)
+                    /*.setBoundsBias(new LatLngBounds(
+                            new LatLng(-33.880490, 151.184363),
+                            new LatLng(-33.858754, 151.229596)))*/
+                    .build(this);
+            startActivityForResult(intent, requestCode);
+        } catch (GooglePlayServicesRepairableException e) {
+            // Indicates that Google Play Services is either not installed or not up to date. Prompt
+            // the user to correct the issue.
+            GoogleApiAvailability.getInstance().getErrorDialog(this, e.getConnectionStatusCode(),
+                    0 /* requestCode */).show();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // Indicates that Google Play Services is not available and the problem is not easily
+            // resolvable.
+            String message = "Google Play Services is not available: " +
+                    GoogleApiAvailability.getInstance().getErrorString(e.errorCode);
+
+            Log.e(TAG, message);
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                // Get the user's selected place from the Intent.
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                Log.i(TAG, "Place Selected: " + place.getName());
+                Log.i(TAG, "Place Selected lat and long: " + place.getLatLng());
+                // Format the place's details and display them in the TextView.
+
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                Log.e(TAG, "Error: RESULT_ERROR Status = " + status.toString());
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.d(TAG, "onActivityResult: RESULT_CANCELED ");
+                // Indicates that the activity closed before a selection was made. For example if
+                // the user pressed the back button.
+            }
+        }
     }
 }
